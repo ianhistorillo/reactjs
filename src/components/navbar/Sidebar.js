6b97b3aba3dcb8a32b7a87681bc4a98c9eb6c10d
Aubs <yuson.obz@gmail.com>
2020-12-24 10:17:30 +0800

    First Commit

--- src/components/navbar/Sidebar.js
+++ src/components/navbar/Sidebar.js
@@ -119,6 +119,8 @@ export default function Sidebar() {
                 <li> Workout Categories </li>
                 <li> Payments  </li>
                 <li> Incident Reports  </li>
+                <li> Payments  </li>
+                <li> Logout  </li>
             </Ul>
         </Div>
     );
